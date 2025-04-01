caf4887d7ac826ff82ade577b20965417944899e
noethrone <noe.pereira.epitech.eu>
2025-04-01 11:23:43 +0200

    Add more covers

--- musicine/src/data/albums_data.ts
+++ musicine/src/data/albums_data.ts
@@ -33,6 +33,38 @@ const data: Album[] = [
     artist_name: "Kanye West",
     release_date: "November 22, 2010",
     link_genius: "https://genius.com/albums/Kanye-west/My-beautiful-dark-twisted-fantasy",
+  },
+  {
+    cover: "quaranta.jpg",
+    background: "quaranta.jpg",
+    album_name: "Quaranta",
+    artist_name: "Danny Brown",
+    release_date: "November 17, 2023",
+    link_genius: "https://genius.com/albums/Danny-brown/Quaranta",
+  },
+  {
+    cover: "life_of_pablo.jpg",
+    background: "life_of_pablo.png",
+    album_name: "The Life of Pablo",
+    artist_name: "Kanye West",
+    release_date: "February 14, 2016",
+    link_genius: "https://genius.com/albums/Kanye-west/The-life-of-pablo",
+  },
+  {
+    cover: "chromakopia.jpg",
+    background: "chromakopia.jpg",
+    album_name: "CHROMAKOPIA",
+    artist_name: "Tyler, The Creator",
+    release_date: "October 28, 2024",
+    link_genius: "https://genius.com/albums/Tyler-the-creator/Chromakopia",
+  },
+  {
+    cover: "donda.jpg",
+    background: "donda.png",
+    album_name: "Donda",
+    artist_name: "Kanye West",
+    release_date: "August 29, 2021",
+    link_genius: "https://genius.com/albums/Kanye-west/Donda",
   }
 ]
 
