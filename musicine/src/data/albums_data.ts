b284ab7f160ef430a1e048274b1f99efe6a1a1bd
noethrone <noe.pereira.epitech.eu>
2025-04-03 14:26:53 +0200

    add more covers

--- musicine/src/data/albums_data.ts
+++ musicine/src/data/albums_data.ts
@@ -114,7 +114,127 @@ const data: Album[] = [
     artist_name: "Dr. Dre",
     release_date: "December 15, 1992",
     link_genius: "https://genius.com/albums/Eminem/The-marshall-mathers-lp",
-  }
+  },
+  {
+    album_name: "I LAY DOWN MY LIFE FOR YOU",
+    artist_name: "JPEGMAFIA",
+    release_date: "August 1, 2024",
+    link_genius: "https://genius.com/albums/Jpegmafia/I-lay-down-my-life-for-you",
+  },
+  {
+    album_name: "And Then You Pray For Me",
+    artist_name: "Westside Gunn",
+    release_date: "October 13, 2023",
+    link_genius: "https://genius.com/albums/Westside-gunn/And-then-you-pray-for-me",
+  },
+  {
+    album_name: "Pray for Paris",
+    artist_name: "Westside Gunn",
+    release_date: "April 17, 2020",
+    link_genius: "https://genius.com/albums/Westside-gunn/Pray-for-paris",
+  },
+  {
+    album_name: "Without Warning",
+    artist_name: "21 Savage, Offset & Metro Boomin",
+    release_date: "October 31, 2017",
+    link_genius: "https://genius.com/albums/21-savage-offset-and-metro-boomin/Without-warning",
+  },
+  {
+    album_name: "UTOPIA",
+    artist_name: "Travis Scott",
+    release_date: "July 28, 2023",
+    link_genius: "https://genius.com/albums/Travis-scott/Utopia",
+  },
+  {
+    album_name: "ASTROWORLD",
+    artist_name: "Travis Scott",
+    release_date: "August 3, 2018",
+    link_genius: "https://genius.com/albums/Travis-scott/Astroworld",
+  },
+  {
+    album_name: "Rodeo",
+    artist_name: "Travis Scott",
+    release_date: "September 4, 2015",
+    link_genius: "https://genius.com/albums/Travis-scott/Rodeo",
+  },
+  {
+    album_name: "Days Before Rodeo",
+    artist_name: "Travis Scott",
+    release_date: "August 18, 2014",
+    link_genius: "https://genius.com/albums/Travis-scott/Days-before-rodeo",
+  },
+  {
+    album_name: "Blonde",
+    artist_name: "Frank Ocean",
+    release_date: "August 20, 2016",
+    link_genius: "https://genius.com/albums/Frank-ocean/Blonde",
+  },
+  {
+    album_name: "channel ORANGE",
+    artist_name: "Frank Ocean",
+    release_date: "July 10, 2012",
+    link_genius: "https://genius.com/albums/Frank-ocean/Channel-orange",
+  },
+  {
+    album_name: "Illmatic",
+    artist_name: "Nas",
+    release_date: "April 19, 1994",
+    link_genius: "https://genius.com/albums/Nas/Illmatic",
+  },
+  {
+    album_name: "Life is Beautiful",
+    artist_name: "Larry June, 2 Chainz & The Alchemist",
+    release_date: "February 7, 2025",
+    link_genius: "https://genius.com/albums/Larry-june-2-chainz-and-the-alchemist/Life-is-beautiful",
+  },
+  {
+    album_name: "Just As I Am",
+    artist_name: "Bill Withers",
+    release_date: "May 1971",
+    link_genius: "https://genius.com/albums/Bill-withers/Just-as-i-am",
+  },
+  {
+    album_name: "The College Dropout",
+    artist_name: "Kanye West",
+    release_date: "February 10, 2004",
+    link_genius: "https://genius.com/albums/Kanye-west/The-college-dropout",
+  },
+  {
+    album_name: "Late Registration",
+    artist_name: "Kanye West",
+    release_date: "August 30, 2005",
+    link_genius: "https://genius.com/albums/Kanye-west/Late-registration",
+  },
+  {
+    album_name: "How Do You Sleep At Night?",
+    artist_name: "Teezo Touchdown",
+    release_date: "September 8, 2023",
+    link_genius: "https://genius.com/albums/Teezo-touchdown/How-do-you-sleep-at-night",
+  },
+  {
+    album_name: "Graduation",
+    artist_name: "Kanye West",
+    release_date: "September 11, 2007",
+    link_genius: "https://genius.com/albums/Kanye-west/Graduation",
+  },
+  {
+    album_name: "808s & Heartbreak",
+    artist_name: "Kanye West",
+    release_date: "November 24, 2008",
+    link_genius: "https://genius.com/albums/Kanye-west/808s-heartbreak",
+  },
+  {
+    album_name: "Yeezus",
+    artist_name: "Kanye West",
+    release_date: "June 18, 2013",
+    link_genius: "https://genius.com/albums/Kanye-west/Yeezus",
+  },,
+  {
+    album_name: "Kanye West Presents Good Music Cruel Summer",
+    artist_name: "G.O.O.D. Music",
+    release_date: "September 14, 2012",
+    link_genius: "https://genius.com/albums/Good-music/Kanye-west-presents-good-music-cruel-summer",
+  },
 ]
 
 const getAlbums = (): Album[] => {
--- musicine/src/utils/image_utils.ts
+++ musicine/src/utils/image_utils.ts
@@ -1,6 +1,8 @@
 export function getImageURL(name: string, isCover: boolean): string {
     const folder = isCover ? "covers" : "backgrounds";
-    const file_name = name.toLowerCase().replace(/\s+/g, "_");
-    console.log(file_name)
+    const file_name = name.toLowerCase()
+        .replace(/\s+/g, "_")
+        .replace(/[?!,“”]/g, "");
+
     return new URL(`../assets/albums/${folder}/${file_name}.jpg`, import.meta.url).href;
 }
